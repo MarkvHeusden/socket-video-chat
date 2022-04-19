e15173b99d177cc68767b9c0bc404207f068e7d9
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-04-19 19:38:34 +0200

    v1

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 const socket = io('/')
 const myPeer = new Peer(undefined, {
     secure: true,
-    host: 'https://socket-video-chat.herokuapp.com/',
-    port: '443'
+    host: 'socket-video-chat.herokuapp.com',
+    port: '443',
 })
 const mainEl = document.querySelector('main')
 const myVideo = document.createElement('video')
@@ -10,30 +10,32 @@ myVideo.muted = true
 
 const peers = {}
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(camera => {
-    addVideoStream(myVideo, camera)
+navigator.mediaDevices
+    .getUserMedia({
+        video: true,
+        audio: true,
+    })
+    .then((camera) => {
+        addVideoStream(myVideo, camera)
 
-    myPeer.on('call', call => {
-        call.answer(camera)
-        const video  = document.createElement('video')
-        call.on('stream', userVideoStream => {
-            addVideoStream(video, userVideoStream)
+        myPeer.on('call', (call) => {
+            call.answer(camera)
+            const video = document.createElement('video')
+            call.on('stream', (userVideoStream) => {
+                addVideoStream(video, userVideoStream)
+            })
+        })
+
+        socket.on('user-connected', (userId) => {
+            connectToNewUser(userId, camera)
         })
     })
 
-    socket.on('user-connected', userId => {
-        connectToNewUser(userId, camera)
-    })
-})
-
-socket.on('user-disconnected', userId => {
+socket.on('user-disconnected', (userId) => {
     if (peers[userId]) peers[userId].close()
 })
 
-myPeer.on('open', userId => {
+myPeer.on('open', (userId) => {
     socket.emit('join-room', roomId, userId)
 })
 
@@ -45,8 +47,8 @@ function addVideoStream(video, camera) {
 
 function connectToNewUser(userId, camera) {
     const call = myPeer.call(userId, camera)
-    const video  = document.createElement('video')
-    call.on('stream', userVideoStream => {
+    const video = document.createElement('video')
+    call.on('stream', (userVideoStream) => {
         addVideoStream(video, userVideoStream)
     })
     call.on('close', () => video.remove())
